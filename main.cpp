0c2de62a4118656a8780ff6e5354283f16cd35ff
FuruyaYuto <k024g0025@g.neec.ac.jp>
2025-04-25 14:26:03 +0900

    座標変換の修正

--- main.cpp
+++ main.cpp
@@ -40,11 +40,13 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 
 // ベクトルの座標変換
 Vector3 Transform(const Vector3& v, const Matrix4x4& m) {
-    Vector3 result{};
-    result.x = v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + m.m[3][0];
-    result.y = v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + m.m[3][1];
-    result.z = v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + m.m[3][2];
-    return result;
+    float x = v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0] + 1.0f * m.m[3][0];
+    float y = v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1] + 1.0f * m.m[3][1];
+    float z = v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2] + 1.0f * m.m[3][2];
+    float w = v.x * m.m[0][3] + v.y * m.m[1][3] + v.z * m.m[2][3] + 1.0f * m.m[3][3];
+
+    assert(w != 0.0f); // 0除算防止
+    return { x / w, y / w, z / w };
 }
 
 // ---------- 描画用ユーティリティ ----------
